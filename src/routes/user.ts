c9f6e75944dca884e21670e920261eba3f767b4d
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-20 13:56:02 +0545

    updates routes with controllers

---
+++ src/routes/user.ts
@@ -0,0 +1,18 @@
+import { Router, Response, Request } from "express/";
+import {
+  createUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/users";
+
+const userRouter = Router();
+
+const responseTest = (req: Request, res: Response) => {
+  res.send(req.hostname + req.baseUrl + req.url);
+};
+userRouter.route("/").get(getAllUser).post(createUser);
+userRouter.route("/:id").get(getUser).put(updateUser).delete(responseTest);
+userRouter.post("/login", responseTest);
+
+export default userRouter;
