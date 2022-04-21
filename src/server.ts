9d3653e2b039732da04f073bc24cc98e52d715e4
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-21 07:33:42 +0545

    install cors and jwt and implements cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import config from "./config/config";
 import userRouter from "./routes/userRoute";
 
 const app: Express = express();
 config.connectDatabase();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
