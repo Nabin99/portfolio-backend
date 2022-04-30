2f4df69d6e39141efc86fc68207403611689f532
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-30 18:16:11 +0545

    Added real gmail for mailing in mailer function and added public.env file

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,28 +1,24 @@
 import nodemailer from "nodemailer";
-import { Request,Response } from "express";
 import "dotenv/config";
+
+let transporter = nodemailer.createTransport({
+  service:process.env.EMAIL_SERVICE_PROVIDER,
+   auth: {
+     user: process.env.MAILER_EMAIL,
+     pass: process.env.MAILER_PASSWORD,
+   },
+ });
 
 export async function mailer(mailTo: string, name: string) {
   try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: 2525,
-      secure: false,
-      auth: {
-        user: process.env.USER,
-
-        pass: process.env.PASSWORD,
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: "kopiras609@3dinews.com",
+    await transporter.sendMail({
+      from: process.env.MAILER_EMAIL,
       to: mailTo,
       subject: "Message Received ✔",
       text: `Received your message at ${new Date().toDateString()}. 
-    Thank you ${name} for showing your interest on me. I will look through your message and contact you back as soon as possible.
-    sincerely,
-    Nabin Dhital`,
+            Thank you ${name} for showing your interest on me. I will look through your message and contact you back as soon as possible.
+            sincerely,
+            Nabin Dhital`,
       html: `Received your message at <b>${new Date().toDateString()}.</b><br/><br />
     Thank you <b>${name}</b> for showing your interest on me. <br/><br/>I will look through your message and contact you back as soon as possible.<br/><br />
     <i>Sincerely</i>,<br/>
@@ -35,26 +31,21 @@ export async function mailer(mailTo: string, name: string) {
   }
 }
 export async function mailerAdmin({name,email,message}:{name:string,email:string,message:string}) {
-  console.log(name,email,message)
   try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: 2525,
-      secure: false,
-      auth: {
-        user: process.env.USER,
-        pass: process.env.PASSWORD,
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: "kopiras609@3dinews.com",
-      to: process.env.EMAIL,
-      subject: "Message Received ✔",
+    await transporter.sendMail({
+      from: process.env.MAILER_EMAIL,
+      to: process.env.ADMIN_EMAIL,
+      subject: "New Message Received ✔",
       text: `Received message at ${new Date().toDateString()}.
-        Sender Name: ${name} , email Address: ${email} ,[ ${message} ]`,
+        Sender Name: ${name} , email Address: ${email},[ ${message} ]`,
       html: `Received message at <b>${new Date().toDateString()}.</b><br/><br />
-      Sender Name: ${name} ,<br/> email Address: ${email} <br/> ,[ ${message} ] `,
+      <b>Sender Name:</b> <i>${name},</i><br/><br/>
+      <b>Email ID:</b> <i>${email},</i> <br/><br/>
+       <b>Message:</b><br/>
+       {<br/>
+        ${message}
+        <br/>
+      }`,
     });
     return true;
   } catch (err) {
